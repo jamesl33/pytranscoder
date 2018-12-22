6be0c76355c6282e085b13eb1c5c8dbf38825f66
James Lee <jamesl33info@gmail.com>
2018-12-22 14:32:22 +0000

    Added setup.py so pytranscoder can be packaged

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='pytranscoder',
+      version='0.1',
+      author='James Lee',
+      author_email='jamesl33info@gmail.com',
+      packages=['.'],
+      install_requires=[
+          'pyaml',
+          'pyyaml',
+          'pprint'
+      ])

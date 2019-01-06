5a386c832f85d20dccfc4ca035a29660888780b3
James Lee <jamesl33info@gmail.com>
2019-01-06 18:17:52 +0000

    Improve workers. Write store file on mark_transcoded(self).

--- pytranscoder.py
+++ pytranscoder.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import queue
 import subprocess
 import threading
-import queue
 
 from typing import List
 
-import yaml
 import pyaml
-import pprint
+import yaml
 
 
 TRANSCODE_WORKERS = 8
-DEFAULT_TRANSCODE_COUNT = 25
 DEFAULT_MEDIA_DIR = '/mnt/storage/media'
 DEFAULT_STORE_FILENAME = '~/.pytranscoder.yml'
 SUPPORTED_FILE_EXTENSIONS = ['.mp4', '.avi', '.mkv']
@@ -31,8 +29,31 @@ def get_media_files(directory: str) -> List[str]:
 
 
 class Store:
-    def __init__(self, mediaDir: str) -> None:
-        self.mediaDir = mediaDir
+    def __init__(self, filename: str, mediaDir: str) -> None:
+        self._filename = os.path.expanduser(filename)
+        self._mediaDir = mediaDir
+
+        # Open and update the store
+        try:
+            with open(self._filename, 'r') as infile:
+                self._data = yaml.load(infile)
+        except FileNotFoundError:
+            self._data = {
+                'transcoded': [],
+                'untranscoded': []
+            }
+
+        # Update the store once we have opened it
+        knownFiles = self._data['transcoded'] + self._data['untranscoded']
+        mediaFiles = get_media_files(self._mediaDir)
+
+        for file in mediaFiles:
+            if file not in knownFiles:
+                self._data['untranscoded'].append(file)
+
+        # Save the updated file
+        with open(self._filename, 'w') as outfile:
+            pyaml.dump(self._data, outfile)
 
     @property
     def transcoded(self) -> List[str]:
@@ -42,41 +63,20 @@ class Store:
     def untranscoded(self) -> List[str]:
         return self._data['untranscoded']
 
-    def open(self, filename: str) -> None:
-        try:
-            with open(filename, 'r') as file:
-                self._data = yaml.load(file)
-        except FileNotFoundError:
-            self._data = {
-                'transcoded': [],
-                'untranscoded': []
-            }
-
-    def close(self, filename: str) -> None:
-        with open(filename, 'w') as file:
-            pyaml.dump(self._data, file)
-
-    def update(self) -> None:
-        knownFiles = self._data['transcoded'] + self._data['untranscoded']
-        mediaFiles = get_media_files(self.mediaDir)
-
-        for file in mediaFiles:
-            if file not in knownFiles:
-                self._data['untranscoded'].append(file)
-
     def mark_transcoded(self, file: str) -> None:
         self._data['untranscoded'].remove(file)
         self._data['transcoded'].append(file)
 
-    def __repr__(self):
-        return pprint.pformat(self._data)
+        with open(self._filename, 'w') as outfile:
+            pyaml.dump(self._data, outfile)
 
 
-def transcode_worker(transcodeQueue: queue.Queue) -> None:
+def transcode_worker(store: Store, transcodeQueue: queue.Queue) -> None:
     while True:
-        file = transcodeQueue.get()
-
-        if file is None:
+        try:
+            file = transcodeQueue.get(timeout=5)
+        except queue.Empty:
+            # Stop the worker if there isn't any more items to transcode
             break
 
         subprocess.call('ffmpeg -i "{0}" -map_chapters -1 -map_metadata -1 -metadata:s:a language=eng -metadata:s:v language=eng -sn -profile:v high -level:v 4.0 -acodec aac -vcodec h264 "$(dirname \"{0}\")/$(basename \"{0}\" .mp4).transcoding.mp4"'.format(file), shell=True)
@@ -87,21 +87,22 @@ def transcode_worker(transcodeQueue: queue.Queue) -> None:
         transcodeQueue.task_done()
 
 
-if __name__ == '__main__':
+def transcode_files(count: int) -> None:
+    store = Store(DEFAULT_STORE_FILENAME, DEFAULT_MEDIA_DIR)
+
     transcodeQueue = queue.Queue()
+
+    for file in store.untranscoded[:count]:
+        transcodeQueue.put(file)
 
     for _ in range(TRANSCODE_WORKERS):
         thread = threading.Thread(target=transcode_worker,
-                                  args=(transcodeQueue,))
+                                  args=(store, transcodeQueue))
         thread.setDaemon(True)
         thread.start()
 
-    store = Store(DEFAULT_MEDIA_DIR)
-    store.open(os.path.expanduser(DEFAULT_STORE_FILENAME))
-    store.update()
-
-    for file in store.untranscoded[:DEFAULT_TRANSCODE_COUNT]:
-        transcodeQueue.put(file)
-
     transcodeQueue.join()
-    store.close(os.path.expanduser(DEFAULT_STORE_FILENAME))
+
+
+if __name__ == '__main__':
+    transcode_files(25)
